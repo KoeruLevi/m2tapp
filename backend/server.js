c1fddfafd3ef8274ef86fa8516ac8ce689d34a15
Hector Toro <asdxdd7@gmail.com>
2025-07-31 10:18:13 -0400

    pres

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,10 @@ connectDB()
 app.use('/api/data', dataRoutes);
 app.use('/api/auth', authRoutes);
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, './frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, './frontend/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 80;
